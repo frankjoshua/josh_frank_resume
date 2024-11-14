420492a2f86bedea17e4bb999b7c0d23fde27c61
Joshua Frank <josh@joshfrank.com>
2024-11-13 21:11:51 -0600

    Added parallax background

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,33 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/parallax.dart';
 import 'package:flutter/material.dart';
 
+import 'resume_world.dart';
+
 void main() {
-  final game = FlameGame();
+  final game = ResumeGame();
   runApp(GameWidget(game: game));
+}
+
+class ResumeGame extends FlameGame {
+  final _imageNames = [
+    ParallaxImageData('parallax/bg.png'),
+    ParallaxImageData('parallax/mountain-far.png'),
+    ParallaxImageData('parallax/mountains.png'),
+    ParallaxImageData('parallax/trees.png'),
+    ParallaxImageData('parallax/foreground-trees.png'),
+  ];
+  
+  @override
+  Future<void> onLoad() async {
+    final parallax = await loadParallaxComponent(
+      _imageNames,
+      baseVelocity: Vector2(20, 0),
+      velocityMultiplierDelta: Vector2(1.8, 1.0),
+      filterQuality: FilterQuality.none,
+    );
+    add(parallax);
+    await add(ResumeWorld());
+  }
 }
---
+++ lib/resume_world.dart
@@ -0,0 +1,8 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+
+class ResumeWorld extends World {
+  @override
+  Future<void> onLoad() async {}
+}
